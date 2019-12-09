7c5f1542cba4d4fed91cb1f406b3947f7af6e064
Rachit Kataria <rachitka@usc.edu>
2019-12-08 19:19:28 -0800

    Piped together all routes and final scoring

--- app/routes.py
+++ app/routes.py
@@ -12,17 +12,18 @@ QUERY_FOLDER = os.path.join(app.root_path, '../audio/mfcc/data')
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+    return "Hello, World!"
 
 @app.route('/audio', methods=['POST'])
 def audio():
-	if request.method == 'POST':
-		# Get file name and file path
-		filename = request.get_json()['filename']
-		file_path = os.path.join(app.root_path, '../audio/mfcc/query', filename)
-		# Retrieve ranked wavs for query
-		wav_scores = mfcc.get_wavs_scores_for_query(file_path)
-		return jsonify(wav_scores)
+    if request.method == 'POST':
+        print(request.__dict__)
+        # Get file name and file path
+        filename = request.get_json()['filename']
+        file_path = os.path.join(app.root_path, '../audio/mfcc/query', filename)
+        # Retrieve ranked wavs for query
+        wav_scores = mfcc.get_wavs_scores_for_query(file_path)
+        return jsonify(wav_scores)
 
 @app.route('/convert_to_mp4', methods=['POST'])
 def convert_to_mp4():
@@ -43,13 +44,13 @@ def color():
             histogram_comparison.get_matches(
                 os.path.join(app.root_path, '../data/query', request.get_json()['filename']),
             ),
-        )	
+        )   
 
 @app.route('/edge_detection', methods=['POST'])
 def edge_detection():
-	if request.method == 'POST':
-		return jsonify(
-			detect.online_processing(
-				os.path.join(app.root_path, request.get_json()['filename']),
-			),
-		)
+    if request.method == 'POST':
+        return jsonify(
+            detect.online_processing(
+                os.path.join(app.root_path, request.get_json()['filename']),
+            ),
+        )
